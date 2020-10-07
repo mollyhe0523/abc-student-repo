af928b9de6d224f1e8be6f6faad8a628a9a757b9
何维翊 <molly@heweiyideMacBook-Pro.local>
2020-10-08 00:58:21 +0800

    mini 3

--- projects/mini-projects/3/script-new.js
+++ projects/mini-projects/3/script-new.js
@@ -10,16 +10,21 @@
 //     console.log("stop");
 //   },2000)
 // })
+// alert("1");
+let pop= document.getElementById("pop");
+pop.loop = false;
 
 let wheat = document.getElementsByClassName("wheat");
 for (let i=0;i<wheat.length;i++){
   // wheat[i].index = i;
   wheat[i].addEventListener("mouseover",()=>{
     console.log("yo");
-    wheat[i].style.animation = "skew 2s linear";
+    wheat[i].style.animation = "skew 1s linear";
+    pop.play();
   });
   wheat[i].addEventListener("mouseout",()=>{
     setTimeout(()=>{
+      pop.pause();
       wheat[i].style.animation = "none";
       console.log("stop");
     },2000)
