41800f664620ec7a6d16b24f2a47e89c7bbb4ceb
何维翊 <molly@heweiyideMacBook-Pro.local>
2020-10-26 22:34:01 +0800

    pj B prototype

---
+++ projects/in-class/chrome-extension/bg-click/contentscript.js
@@ -0,0 +1,14 @@
+// console.log("Yo what's up");
+// //
+// // function replace(word1,word2){
+// //   let regEx = new RegExp(word1,"g");
+// //   document.body.innerHTML = document.body.innerHTML.replace(regEx,word2);
+// // }
+//
+//
+// function gotMessage(request,sender,sendResponse){
+//   console.log(request);
+//   // replace(message.word1,message.word2);
+//
+// }
+// chrome.runtime.onMessage.addListener(gotMessage);
---
+++ projects/in-class/chrome-extension/bg-click/popup/script.js
@@ -0,0 +1,20 @@
+let button=document.getElementById("button");
+let valueDisplay=document.getElementById("currentValue");
+
+let currentValue = 0;
+
+chrome.runtime.sendMessage({type:"getCurrentValue"}, function(response){
+  console.log("response is", response);
+  currentValue = response.value;
+  valueDisplay.innerHTML=currentValue;
+
+});
+
+
+
+button.addEventListener("click",()=>{
+  currentValue++;
+  valueDisplay.innerHTML=currentValue;
+
+  chrome.runtime.sendMessage({type:"increaseValue"})
+})
