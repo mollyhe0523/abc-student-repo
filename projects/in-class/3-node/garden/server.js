daaf2f36178d846f59b021f5cde64e10c4e62763
何维翊 <molly@heweiyideMacBook-Pro.local>
2020-11-19 00:24:08 +0800

    garden in-class

---
+++ projects/in-class/3-node/garden/server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const port = 3010
+const secret = "paperclip";
+// const port = process.env.PORT
+let gifts = [];
+
+app.use(express.static('public'))
+
+app.get('/secret',(req,res)=>{
+  let query = req.query;
+  let guess = query.word;
+  if (guess == secret){
+    console.log("go in")
+    res.redirect("/garden")
+    // res.sendFile(__dirname+"/public/garden/index.html")
+  }else{
+    console.log("NO")
+    res.redirect("/fishy")
+    // res.sendFile(__dirname+"/public/fishy/index.html")
+  }
+  console.log("--------")
+})
+
+app.get('/gift',(req,res)=>{
+  let query = req.query;
+  let gift = query.gift;
+  gifts.push(gift);
+  console.log("received:",gift);
+  console.log("all the gifts:",gifts);
+
+})
+
+app.get('/getGifts',(req,res)=>{
+  console.log("someone asks for gifts")
+  res.json({content:gifts,sender:"the garden gods"});
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ projects/in-class/3-node/web-socket/server.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected',socket.id);
+  // console.log('id',socket.id);
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected',socket.id);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
