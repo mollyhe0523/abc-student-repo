e9a0399714d381accc2f5d3c34bddb192a5f43ee
何维翊 <molly@heweiyideMacBook-Pro.local>
2020-11-23 23:46:45 +0800

    PJ C Proto

---
+++ projects/in-class/3-node/web-socket/public/script.js
@@ -0,0 +1,46 @@
+console.log("hi");
+
+let socket = io();
+let namebox = document.getElementById('name');
+let chatbox = document.getElementById('chat');
+let messagebox = document.getElementById('message');
+let sendbutton = document.getElementById('send');
+
+sendbutton.addEventListener("click",()=>{
+  console.log("click");
+  let name=namebox.value.trim();
+  if (name ==""){
+    name="anonymous";
+    namebox.value = "";
+  }
+  let message = messagebox.value.trim();
+  // console.log("message is: ",message);
+  if (message != ""){
+    let data = {name:name, message: message};
+    socket.emit('message',data);
+    console.log(data);
+  }
+  messagebox.value = "";
+})
+
+
+socket.on("incoming",(data)=>{
+  console.log(data);
+  let name= data.name;
+  let message = data.message;
+  let li = document.createElement("li");
+  let p = document.createElement("p");
+  p.innerHTML = "<span class = 'sender'>"+name+":</span> "+message;
+  li.appendChild(p);
+  chatbox.appendChild(li);
+
+  //check
+  //html{scroll-behavior:smooth}
+  chatbox.scrollTop = chatbox.scrollHeight;
+})
+
+messagebox.addEventListener("keyup",(event)=>{
+  if (event.keyCode == 13){
+    sendbutton.click();
+  }
+})
--- projects/in-class/3-node/web-socket/server.js
+++ projects/in-class/3-node/web-socket/server.js
@@ -1,16 +1,20 @@
-var app = require('express')();
+let express =require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 io.on('connection', (socket) => {
   console.log('a user connected',socket.id);
   // console.log('id',socket.id);
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+  socket.on('message', (msg) => {
+    console.log(msg);
+    io.emit("incoming",msg);
   });
   socket.on('disconnect', () => {
     console.log('user disconnected',socket.id);
